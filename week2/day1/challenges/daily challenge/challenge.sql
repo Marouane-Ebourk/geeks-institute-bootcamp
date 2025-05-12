38b0d266bf95d9a56e276ea32b54304378770d3f
Marouane-Ebourk <marouaneebourk@gmail.com>
2025-05-12 16:45:13 +0100

    Week 2 Day 1: Add solution for Daily Challenge (Actors)

---
+++ week2/day1/challenges/daily challenge/challenge.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*) FROM actors;
+
+-- Try to add a new actor with some blank fields. What do you think the outcome will be ?
+-- It will not work because some fields have the "NOT NULL" constraint
