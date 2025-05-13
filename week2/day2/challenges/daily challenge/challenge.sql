7c52bd420753685610cba5f33323ffb972f2c8d1
Marouane-Ebourk <marouaneebourk@gmail.com>
2025-05-13 12:25:51 +0100

    Week 2 Day 2: Add solution for Daily Challenge : SQl Puzzle

---
+++ week2/day2/challenges/daily challenge/challenge.sql
@@ -0,0 +1,29 @@
+/*
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+*/
+
+-- output: 0
+
+/*
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+*/
+
+-- output: 2
+
+/*
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+*/
+
+-- output: 0
+
+/*
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+*/
+
+-- output: 2
+
+
